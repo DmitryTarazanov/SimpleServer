ac7452185c46b03b29e31b6658ab2d671717ac50
TarazanovDG <tarazanovdima@gmail.com>
2023-02-12 19:33:34 +0700

    Initial commit

---
+++ src/Server.java
@@ -0,0 +1,24 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        String host="127.0.0.1";
+        int port=8089;
+
+        try(Socket clientSocket = new Socket(host,port);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))){
+            System.out.println("New connection");
+            String resp = in.readLine();
+            System.out.println(resp);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
